873db9a8f1d33c5e22008ac46a5627ec5e707894
Richard-D-Paz <152341881+Richard-D-Paz@users.noreply.github.com>
2023-12-16 11:20:37 -0300

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,9 @@
+valorReal = float(input("Digite o valor em reais para se convertido: "))
+
+print("Qual a moeda a sser convertida? / dolarAmericano / euro / libraEsterlina / pesoArgentino / pesoChileno")
+
+cotação = input("Digite exatamente como acima: ")
+
+
+if cotação == "dolarAmericano":
+    print(23)
